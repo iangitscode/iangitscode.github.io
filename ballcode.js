7e4a32ac9adf62fe8f62f9534ab49818f6eb23ce
Ian Lai <i3lai@uwaterloo.ca>
2017-01-12 13:39:50 -0500

    Added rotation of the ball and nicer text

--- ballcode.js
+++ ballcode.js
@@ -2,6 +2,8 @@ var aX=1;
 var aY=1;
 var vX=0;
 var vY=0;
+var angle=0;
+var rotatespeed=0;
 var yPos=window.innerHeight-200;
 var xPos=window.innerWidth/2-100;
 var timer;
@@ -17,14 +19,20 @@ function tick(){
 	document.getElementById("ball").style.top=yPos+"px";
 	document.getElementById("ball").style.left=xPos+"px";
 	if(!started) return;
+
+	angle+=rotatespeed;
+	document.getElementById("ball").style.transform="rotate("+angle+"deg)";
+
+
 	if(xPos+200>window.innerWidth || xPos<0){
 		vX*=-1;
+		rotatespeed*=-1;
 	}
 	yPos+=vY;
 	xPos-=vX;
 	vY+=aY;
 	if(yPos>window.innerHeight+200){ //reset
-		console.log("reset");
+		rotatespeed=0;
 		yPos=window.innerHeight-200;
 		xPos=window.innerWidth/2-100;
 		vX=0;
@@ -36,7 +44,7 @@ function tick(){
 }
 
 function drawScore(){
-	document.getElementById("score").innerHTML="Score: "+score;
+	document.getElementById("score").innerHTML=score;
 }
 
 function clicked(event){
@@ -44,12 +52,13 @@ function clicked(event){
 	score++;
 	if(score>highscore){
 		highscore=score;
-		document.getElementById("highscore").innerHTML="Current High Score: "+highscore;
+		document.getElementById("highscore").innerHTML=highscore;
 	}
 	drawScore();
 	vY=-30;
 	mouseX=event.clientX;
 	if(mouseX>=xPos && mouseX<=xPos+200){
 		vX=(mouseX-xPos-100)/5;
+		rotatespeed=-vX/2;
 	}
 }

27258b7c48bf64d7faf7a92bc464501481310e02
Ian Lai <i3lai@uwaterloo.ca>
2017-01-24 18:17:16 -0500

    Added a slider bar to control game speed, can now only click on the ball when its falling

--- ballcode.js
+++ ballcode.js
@@ -1,85 +1,115 @@
-var aX=1;
-var aY=1;
-var vX=0;
-var vY=0;
-var angle=0;
-var rotatespeed=0;
-var size=window.innerHeight/4;
-var yPos=window.innerHeight-size;
-var xPos=(window.innerWidth-size)/2;
 var timer;
 var score=0;
 var highscore=0;
 var started=false;
+var tickCounter=0;
+var gameSpeed=20;
 
 function init(){
-	timer = setInterval(function(){tick()}, 10);
-	document.getElementById("ball").style.height=size+'px';
-	document.getElementById("ball").style.width=size+'px';
-	createBall();
+	timer = setInterval(function(){tick()}, gameSpeed);
+	var ball=document.getElementById("ball");
+	ball.aX=1;
+	ball.aY=1;
+	ball.vX=0;
+	ball.vY=0;
+	ball.angle=0;
+	ball.rotatespeed=0;
+	ball.size=window.innerHeight/4;
+	ball.yPos=window.innerHeight-ball.size;
+	ball.xPos=(window.innerWidth-ball.size)/2;
+	document.getElementById("ball").style.height=ball.size+'px';
+	document.getElementById("ball").style.width=ball.size+'px';
+	gameSpeed=document.getElementById("slider").value;
 }
 
 function instructions(){
 	var over = document.createElement('div');
+	var slider=document.getElementById("slider");
 	over.innerHTML="Click the ball to kick it! Juggle as long as you can~ <br> (Click anywhere to return)";
 	over.setAttribute('class','over text');
-	over.setAttribute('onclick','this.parentNode.removeChild(this)');
+	over.setAttribute('onclick','uninstructions(this)');
+	descBox.style.display="block";
+	slider.style.display="block";
 	document.body.appendChild(over);
 }
 
-function tick(){
-	
-	document.getElementById("ball").style.top=yPos+"px";
-	document.getElementById("ball").style.left=xPos+"px";
+function uninstructions(obj){
+	obj.parentNode.removeChild(obj);
+	gameSpeed=slider.value/10;
+	clearInterval(timer);
+	timer = setInterval(function(){tick()}, gameSpeed);
+	descBox.style.display="none";
+	slider.style.display="none";
+}
 
+function tick(){
+	var ball=document.getElementById("ball");
+	ball.style.top=ball.yPos+"px";
+	ball.style.left=ball.xPos+"px";
+
+	//if the game hasn't started, do nothing
 	if(!started) return;
 
 	//angle stuff
-	angle+=rotatespeed;
-	document.getElementById("ball").style.transform="rotate("+angle+"deg)";
+	ball.angle+=ball.rotatespeed;
+	ball.style.transform="rotate("+ball.angle+"deg)";
 
 	//bouncing off walls, need to flip xVel and rotatespeed
-	if(xPos+size>window.innerWidth || xPos<0){
-		vX*=-1;
-		rotatespeed*=-1;
+	if(ball.xPos+ball.size>window.innerWidth || ball.xPos<0){
+		ball.vX*=-1;
+		ball.rotatespeed*=-1;
 	}
 
 	//calculate new position
-	yPos+=vY;
-	xPos-=vX;
-	vY+=aY;
+	ball.yPos+=ball.vY;
+	ball.xPos-=ball.vX;
+	ball.vY+=ball.aY;
 
 	//reset if lose
-	if(yPos>window.innerHeight+size){
-		rotatespeed=0;
-		yPos=window.innerHeight-size;
-		xPos=(window.innerWidth-size)/2;
-		vX=0;
-		vY=0;
+	if(ball.yPos>window.innerHeight+ball.size){
+		ball.rotatespeed=0;
+		ball.yPos=window.innerHeight-ball.size;
+		ball.xPos=(window.innerWidth-ball.size)/2;
+		ball.vX=0;
+		ball.vY=0;
 		drawScore();
 		started=false;
+	}
+}
+
+function clicked(event){
+	var ball=document.getElementById("ball");
+
+	//no spam clicking the ball!
+	if(ball.vY<0) return;
+
+	//reset the score if this is the first click
+	if(!started){
+	score=0;
+	started=true;
+	}
+
+	//add to the score, update highscore if necessary	
+	score++;
+	if(score>highscore){
+		highscore=score;
+	}
+	
+	//draw the score and highscore
+	drawScore();
+
+	//set the ball's y velocity
+	ball.vY=-30;
+
+	//set the ball's x velocity and rotation based on mouse position
+	mouseX=event.clientX;
+	if(mouseX>=ball.xPos && mouseX<=ball.xPos+ball.size){
+		ball.vX=(mouseX-ball.xPos-ball.size/2)/5;
+		ball.rotatespeed=-ball.vX/2;
 	}
 }
 
 function drawScore(){
 	document.getElementById("score").innerHTML=score;
 	document.getElementById("highscore").innerHTML=highscore;
-}
-
-function clicked(event){
-	if(!started){
-	score=0;
-	started=true;
-	}	
-	score++;
-	if(score>highscore){
-		highscore=score;
-	}
-	drawScore();
-	vY=-30;
-	mouseX=event.clientX;
-	if(mouseX>=xPos && mouseX<=xPos+size){
-		vX=(mouseX-xPos-size/2)/5;
-		rotatespeed=-vX/2;
-	}
 }

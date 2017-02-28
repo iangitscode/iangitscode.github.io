6f422c13046ec6ce6517ab4e095c9096898da2b8
snipower <i3lai@edu.uwaterloo.ca>
2017-02-28 16:21:05 -0500

    Added an about me box and a skills table

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-$('.carousel').carousel({
-  interval: 5000
-})
+$(document).ready(function(){
+	fillStars();
+});
+
+//Function to insert stars into elements with class 'star'
+//Glyphicons courtesy of http://glyphicons.com/ :)
+function fillStars(){
+	var stars=$(".stars")
+	$.each(stars, function(index, value) {
+  		var number=parseInt(this.innerHTML);
+  		var fullstar="<span class='glyphicon glyphicon-star'></span>";
+  		var emptystar="<span class='glyphicon glyphicon-star-empty'></span>";
+  		this.innerHTML="";
+  		for(var x=0;x<5;x++){
+  			if(x<number) this.innerHTML+=fullstar;
+  			else this.innerHTML+=emptystar;
+  		}
+	});
+}

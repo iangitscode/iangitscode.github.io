56f8f9b1f093b0cd55e01505ad272b41deafee5f
snipower <i3lai@edu.uwaterloo.ca>
2017-03-06 16:53:23 -0500

    Added a textured background, made project iframe look a little nicer

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ var projects = [
 	 description:"This is a small game I built with vanilla HTML, CSS, and Javascript, based on the Facebook Messenger game.\
 	It features a basic physics engine and a variable difficulty setting."},
 	{link:'graph/index.html',
-	 description:"This a node graphing tool using HTML5 Canvas, building off concepts learned in Math 239.\
+	 description:"This is a node graphing tool built using HTML5 Canvas, based on concepts learned in Math 239.\
 	It features nodes that you can create, delete, move, and rename, and the ability to draw edges between nodes.\
-	The base structure of this project was created by Jonah Dlin."}
+	This project was created in collaboration with Jonah Dlin."}
 ];
 
 function switchFrame(id){
